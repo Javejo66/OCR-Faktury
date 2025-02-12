4276710ebdb1d45ae82ac55016055b40141f11c8
Javejo66 <jzalupski66@gmail.com>
2025-02-12 19:02:50 +0100

    Kontrola instalace Tesseract na Renderu

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+which tesseract || echo "Tesseract is NOT installed!"
 apt-get update && apt-get install -y tesseract-ocr
 export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata/
 uvicorn main:app --host 0.0.0.0 --port 10000
