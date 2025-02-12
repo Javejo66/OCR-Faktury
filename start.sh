c43990a99d001c029e45a28adf87701b80984a26
Javejo66 <jzalupski66@gmail.com>
2025-02-12 21:11:00 +0100

    Přidán Dockerfile a opraven start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,10 @@
 # Kontrola a instalace Tesseract OCR
 if ! command -v tesseract &> /dev/null
 then
-    echo "Tesseract není nainstalován, instaluji..."
-    apt update && apt install -y tesseract-ocr
-else
-    echo "Tesseract je již nainstalován."
+    echo "Tesseract není nainstalován! Ukončuji..."
+    exit 1
 fi
+
 
 # Spuštění aplikace
 uvicorn main:app --host 0.0.0.0 --port 10000
