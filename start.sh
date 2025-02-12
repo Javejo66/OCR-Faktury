e48944471aa6146818f595baf9bd488d82dc046e
Javejo66 <jzalupski66@gmail.com>
2025-02-12 19:48:45 +0100

    Použití lokální verze Tesseractu z repozitáře

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# Aktualizace balíčků a instalace Tesseract OCR
-apt-get update && apt-get install -y tesseract-ocr tesseract-ocr-all
+# Rozbalení Tesseractu z našeho repozitáře
+tar -xzf bin/tesseract-5.5.0.tar.gz -C bin/
 
-# Ověření instalace
-which tesseract || echo "Tesseract stále NENÍ nainstalován!"
+# Nastavení cesty k Tesseractu
+export PATH=$PWD/bin/tesseract-5.5.0:$PATH
+export TESSDATA_PREFIX=$PWD/bin/tesseract-5.5.0/tessdata
 
-# Nastavení proměnné prostředí
-export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata/
+# Kontrola, jestli je Tesseract dostupný
+which tesseract || echo "Tesseract stále NENÍ dostupný!"
 
 # Spuštění aplikace
 uvicorn main:app --host 0.0.0.0 --port 10000
-
